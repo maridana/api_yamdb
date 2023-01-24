a6a9456d7623f7d54cf4ab41d253755b73ee7954
Валентин Горовенко <Punker8381@yandex.ru>
2023-01-24 17:38:47 +0700

    Добавил permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,3 +10,30 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.is_admin or request.user.is_superuser)
+
+
+class AnonimReadOnly(permissions.BasePermission):
+    """Разрешает анонимному пользователю только безопасные запросы."""
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsSuperUserIsAdminIsModeratorIsAuthor(permissions.BasePermission):
+    """
+    Разрешает анонимному пользователю только безопасные запросы.
+    Доступ к запросам PATCH и DELETE предоставляется только
+    суперпользователю, администратору, аутентифицированным пользователям
+    с ролью admin или moderator, и автору.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and (request.user.is_superuser
+                 or request.user.is_staff
+                 or request.user.is_admin
+                 or request.user.is_moderator
+                 or request.user == obj.author)
+        )
