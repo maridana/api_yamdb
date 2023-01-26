4683cc8c8a3321b992896117938ad5ff8da86012
maridana <marina.krasnopevtceva@gmail.com>
2023-01-26 21:28:23 +0300

    Первый блин

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from reviews.models import Category, Genre, Title
+from api.filters import TitleFilter
+from .mixins import ModelMixinSet
+from .serializers import (CategorySerializer, GenreSerializer, 
+                          TitleReadSerializer, TitleWriteSerializer)
+
+class CategoryViewSet(ModelMixinSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class GenreViewSet(ModelMixinSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class TitleViewSet(ModelViewSet):
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')
+    ).all()
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleWriteSerializer
---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,12 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+def validate_year(value):
+    now = timezone.now().year
+    if value > now:
+        raise ValidationError(
+            f'{value} не может быть больше {now}'
+        )
